916b7bc2313443fc3cc1fa0cf428bfb28b076f02
FimastGD <113923761+FimastGD@users.noreply.github.com>
2024-04-28 12:39:05 +0300

    Delete src/launch.js

--- src/launch.js
+++
@@ -1,8 +0,0 @@
-const { ipcRenderer } = require('electron');
-function launch() {
-  ver = "ja12303";
-  ipcRenderer.send('launch', ver);
-}
-document.getElementById('launch').addEventListener('click', launch());
-
-// let etc если етк == альфа 12303 то выполнить испрендер лаунч четатам
