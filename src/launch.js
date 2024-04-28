efb8cd32b6b3c065387125f1f562d22a06c8434e
FimastGD <113923761+FimastGD@users.noreply.github.com>
2024-04-28 14:34:42 +0300

    Update to beta 1.0.1

--- src/launch.js
+++ src/launch.js
@@ -1,5 +1,24 @@
+// Объявляем переменную 'vers' и устанавливаем её начальное значение
+let vers = "ja12303";
+
+// Функция для изменения значения 'vers' и текста кнопки
+function select() {
+  let selectButton = document.getElementById('select');
+  if (vers === "ja12303") {
+    vers = "ja12303rem";
+    selectButton.innerHTML = '<font size="2px">Alpha 1.2.3_03 (Remastered)</font>';
+  } else {
+    vers = "ja12303";
+    selectButton.textContent = 'Alpha 1.2.3_03';
+  }
+}
+
+// Устанавливаем начальный текст кнопки при загрузке страницы
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('select').textContent = 'Alpha 1.2.3_03';
+});
+
 function launch() {
-  vers = "ja12303";
   route = `C:\\creepyproject\\java\\${vers}\\preload.bat`;
   ipcRenderer.send('launch', route);
   console.log('routed');
