ce772bc17dda7429669f8002db425d5056186fc6
FimastGD <113923761+FimastGD@users.noreply.github.com>
2024-05-04 13:25:45 +0300

    [TECH-ONLY] 1.1.2 - $filler

--- java/ja000/load.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd C:\creepytoast\java\ja000
-color 0f
-java -Xmx1024M -Xms1024M -cp Minecraft.jar "-Dorg.lwjgl.librarypath=%CD%/natives" "-Dnet.java.games.input.librarypath=%CD%/natives" Start
-rem pause
-exit
--- java/ja000/preload.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-color 0f
-cd C:\creepytoast\java\ja000
-start load.bat
